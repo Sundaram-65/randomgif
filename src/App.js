bb9fb594cb7844e2bbaeeab81df5e3165c4ba54e
Sundaram-65 <sundaramgupta010203@gmail.com>
2026-01-13 16:25:43 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import { Random } from "./components/Random";
-import { Tag } from "./components/Tag";
+import { Random } from "./components/Random.jsx";
+import { Tag } from "./components/Tag.jsx";
 function App() {
   return (
-    <div className="background flex flex-col min-h-screen w-full">
-      
-      <h1 className="text-3xl font-semibold">RANDOM GIF GENRATOR </h1>
-      <div>
+    <div className="background flex flex-col min-h-screen w-full items-center">
+      <h1 className="text-3xl font-bold bg-white mt-[40px] text-center rounded-xl w-11/12">RANDOM GIF GENRATOR </h1>
+   
         <Random></Random>
         <Tag></Tag>
-      </div>
+      
     </div>
   );
 }
--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Spinner } from './Spinner';
 
 export const Random = () => {
+
+  const [gif, setGif] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+
+  async function fetchData() {
+    setLoading(true);
+
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&timestamp=${Date.now()}`;
+    const { data } = await axios.get(url);
+
+    // Faster loading format
+    const imgURL = data.data.images.fixed_width.webp;
+    setGif(imgURL);
+
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div>Random</div>
-  )
-}
+    <div className='w-1/2 mx-auto bg-green-400  mt-[40px] rounded-xl border-2 border-gray-400 flex flex-col items-center justify-center gap-y-3'>
+
+      <h1 className='underline text-xl uppercase font-bold mt-4'>A RANDOM GIF</h1>
+
+      {loading ? (
+        <Spinner></Spinner>
+      ) : (
+        gif && <img src={gif} alt='GIF' width='450px' height='300px' />
+      )}
+
+      <button
+        onClick={fetchData}
+        className='w-11/12 bg-white opacity-70 rounded-md text-lg py-1 mb-4'
+      >
+        GENERATE
+      </button>
+    </div>
+  );
+};
